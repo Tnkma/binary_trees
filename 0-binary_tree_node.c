0fa6583f6e55b5fc117324f3b77b91c281ff56d7
Tnkma <onwusilikegodswill@gmail.com>
2024-01-03 17:22:51 +0100

    my first push

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - create a new node for the tree
+ * @parent: the root tree
+ * @value: the value of the node
+*/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    //decalre a variable for the new node
+    binary_tree_t *newnode;
+
+    newnode = (binary_tree_t *) malloc(sizeof (binary_tree_t));
+    
+}
